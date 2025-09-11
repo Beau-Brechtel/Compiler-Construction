ca95031d48819df10d805ace6c0fa87a79b5e366
Beau <bmb0121@auburn.edu>
2025-09-10 19:26:38 -0500

    test files

--- test_files/test1.c
+++ test_files/test1.c
@@ -1,6 +1,6 @@
 // Short lexer test
 int main() {
-    int 3x = 10;
+    int x = 10;
     float y = 3.14;
 
     if (x != 0) y = y / x;
---
+++ test_files/test3.c
@@ -0,0 +1,8 @@
+int main() {
+    int i;
+    printf("For loop test:\n");
+    for (i = 0; i < 5; i++) {
+        printf("Iteration %d\n", i);
+    }
+    return 0;
+}
